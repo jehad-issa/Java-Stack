1ce116e72cd844818e5500111bbab6c5809d6805
jehad-issa <96985283+jehad-issa@users.noreply.github.com>
2022-03-04 18:48:49 +0200

    add

---
+++ Java Fundamentals/First java program/Jehad.java
@@ -0,0 +1,7 @@
+public class Jehad {
+	public static void main(String[] args) {
+		System.out.println("My name is jehad");
+        System.out.println("I am 28 years old");
+        System.out.println("My hometown is Nablus, Palestine");
+	}
+}
